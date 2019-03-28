4f51515f66be1e353d0c2b513319aa039c6358fc
Riccardo <riccardo.lorenzon@gmail.com>
2019-03-28 22:17:41 +0100

    Use FlatList

--- src/components/CatsList.js
+++ src/components/CatsList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, FlatList } from 'react-native';
 import axios from 'axios';
 import CatDetail from './CatDetail';
 
 class CatsList extends Component {
-  state = { cats: [] };
+  constructor() {
+    super();
+    this.state = { 
+      cats: [],
+      page: 1 
+    };
+  }
 
   componentWillMount() {
     axios.get('https://api.thecatapi.com/v1/images/search?limit=10&page=1&order=DESC&mime_types=jpg,png')
@@ -17,13 +23,23 @@ class CatsList extends Component {
     );
   }
 
+  loadMoreData() {
+    axios.get('https://api.thecatapi.com/v1/images/search?limit=10&page=1&order=DESC&mime_types=jpg,png')
+      .then(response => this.setState({ cats: [...this.state.cats, ...response.data]}));
+  }
+
+  _renderCat = ({item : cat}) => {
+    return <CatDetail key={cat.id} cat={cat} />;
+  }
+
   render() {
     console.log(this.state);
-
     return (
-      <ScrollView>
-        {this.renderCats()}
-      </ScrollView>
+      <FlatList
+        data={this.state.cats}
+        renderItem={this._renderCat}
+        >
+      </FlatList>
     );
   }
 }
