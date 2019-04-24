6d8add898fb6342e0de65f58097560ed3e3446b0
Riccardo <riccardo.lorenzon@gmail.com>
2019-04-25 00:04:39 +0200

    Cleaning up.

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
 
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import CatsList from './src/components/CatsList';
 import RouterComponent from './src/Router';
-import Header from './src/components/Header';
 
 // Create a component
 const App = () => (
